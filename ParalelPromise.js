cb7ef7a2844e2bb7c3befa0a31cf9a588f7b1546
Rolando Cachipuendo <r_cachipuendo_reca@hotmail.com>
2021-05-01 00:59:40 -0500

    Paralel Promise

---
+++ ParalelPromise.js
@@ -0,0 +1,44 @@
+const API_URL = "https://swapi.dev/api";
+const PEOPLE = "/people/:id";
+
+const luckUrl = `${API_URL}${PEOPLE.replace(":id", 1)}`;
+const opt = { crossDomain: true };
+const onLukeResponse = function (luke) {
+  console.log(`Hola soy ${luke.name}`);
+};
+
+/**
+ * Esto es un poco más refactorizado
+ * @param {code} id of person
+ * @returns {LinkStyle}fullfilled or rejected
+ */
+function obtenerPersonaje(id) {
+  return new Promise((resolve, reject) => {
+    var luckUrl = `${API_URL}${PEOPLE.replace(":id", id)}`;
+    $.get(luckUrl, opt, function (data) {
+      resolve(data);
+    }).fail(() => reject(id));
+  });
+}
+/**
+ *
+ * @param {code} id of person
+ */
+function onError(id) {
+  console.log(`Sucedio un error al obtener el personaje ${id}`);
+}
+
+var ids = [1, 2, 3, 4, 5, 6, 7];
+/* This is with normal function
+var promises = ids.map(function (id) {
+  return obtenerPersonaje(id);
+});
+*/
+
+//This is with arrow function
+var promises = ids.map((id) => obtenerPersonaje(id));
+
+console.log(promises);
+Promise.all(promises)
+  .then((person) => console.log(person))
+  .catch(onError);
